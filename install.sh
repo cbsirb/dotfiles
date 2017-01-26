2630544838066009ab77cc2cace580da43bafef7
rompy <bogdans91@yahoo.com>
2017-01-26 09:47:39 +0200

    Fixed install script with symlinks

--- install.sh
+++ install.sh
@@ -8,33 +8,37 @@ if [ ! -d ".bak" ]; then
 fi
 
 function backup_file {
-    if [ -f "$1" ]; then
+    if [ ! -h "$1" ] && [ -f "$1" ]; then
         mv "$1" .bak/
+    elif [ -h "$1" ]; then
+        rm "$1"
     fi
 }
 
 function backup_dir {
-    if [ -d "$1" ]; then
+    if [ ! -h "$1" ] && [ -d "$1" ]; then
         mv "$1" .bak/
+    elif [ -h "$1" ]; then
+        rm "$1"
     fi
 }
 
 function install_dotfile {
     echo Installing file $1
     backup_file "$HOME/.$1"
-    ln -s "$1" "$HOME/.$1"
+    ln -s "$(realpath $1)" "$HOME/.$1"
 }
 
 function install_dotdir {
     echo Installing directory $1
     backup_dir "$HOME/.$1"
-    ln -s "$1" "$HOME/.$1"
+    ln -s "$(realpath $1)" "$HOME/.$1"
 }
 
 function install_config_dir {
     echo Installing $1
     backup_dir "$HOME/.config/$1"
-    ln -s "$1"  "$HOME/.config/$1"
+    ln -s "$(realpath $1)" "$HOME/.config/$1"
 }
 
 install_dotfile "bash_aliases"
@@ -46,7 +50,9 @@ install_dotfile "bash_profile"
 install_dotfile "bashrc"
 
 install_dotfile "tmux.conf"
-install_dotdir "tmux.d"
+
+backup_dir "$HOME/.tmux.d"
+mkdir "$HOME/.tmux.d"
 git clone https://github.com/tmux-plugins/tmux-yank "$HOME/.tmux.d/tmux-yank"
 
 install_dotfile "Xresources"
