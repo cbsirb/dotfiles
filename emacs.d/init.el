dbdacd675e0c0f6e22c2017daed1391b3f67a692
Cristian-Bogdan SIRB <cbsirb@gmail.com>
2020-10-13 12:26:36 +0300

    elfeed: added drewdevault

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2061,6 +2061,7 @@ found, an error is signaled."
            ("https://virtuallyfun.com/wordpress/feed" tech)
            ("https://stopa.io/feed.rss" tech)
            ("https://kliu.io/index.xml" tech)
+           ("https://drewdevault.com/blog/index.xml" tech)
            ("https://obscuritory.com/feed/" pop)
            ("https://planet.emacslife.com/atom.xml" emacs)
            ("https://www.phoronix.com/rss.php" linux phoronix)
